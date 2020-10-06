0fb82732a5e93ad7902c612bbd6a7d0c12e50406
Ariel K <ar.kogan@gmail.com>
2020-10-06 23:33:24 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,12 +1,12 @@
 //define functions here
 
-$(document).ready(function(){
-
-  function getIt() {
-    $('p').on(click, function) {
-      alert('Hey!')
-    }
+function getIt() {
+  $('p').on(click, function) {
+    alert('Hey!')
   }
+}
 
+$(document).ready(function(){
+  getIt()
 
 });
