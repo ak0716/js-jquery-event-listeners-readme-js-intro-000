5816978b4060f75621a8b511d91b31b6d932ada1
Ariel K <ar.kogan@gmail.com>
2020-10-06 23:46:30 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -20,7 +20,7 @@ function pressIt() {
 }
 
 function submitIt() {
-  $('form:last').on('subit', function() {
+  $('form:last').on('submit', function() {
     alert('Your form is going to be submitted now.')
   })
 }
